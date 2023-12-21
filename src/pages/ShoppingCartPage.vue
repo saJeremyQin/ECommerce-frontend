585c475dd9f302470188aedc49bd88bd96c8af2c
saJeremyQin <343220868@qq.com>
2023-12-21 16:58:12 +1030

    add ShoppingCartList, work well

--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,25 +1,10 @@
 <template>
     <h1>Shopping Cart</h1>
-    <div v-if="cartItems.length > 0">
-        <div 
-            class="product-container"
-            v-for="product in cartItems"
-            :key="product.id"
-        >
-            <img class="product-image" :src="product.imageName" />
-            <div class="details-wrap">
-                <h3>{{ product.name }}</h3>
-                <p>{{ product.price }}</p>
-            </div>
-            <button class="remove-button">Remove from Cart</button>
-        </div>
-        <button class="checkout-button">Proceed to Checkout</button>
-    </div>
-    <div v-else>
-        You have no items in your cart yet.
-    </div>
+    <ShoppingCartList :cartItems="cartItems"/>
 </template>
 
 <script setup>
-import { cartItems } from '@/temp-data';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+// import { cartItems } from '@/temp-data';
+const cartItems = [];
 </script>
