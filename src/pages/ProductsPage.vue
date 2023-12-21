e4cf79077145fc590085f568dc0e3ff40dfb3e37
saJeremyQin <343220868@qq.com>
2023-12-21 16:30:07 +1030

    ProductsList is used. Works well

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,23 +1,9 @@
 <template>
     <h1>Products</h1>
-    <div class="grid-wrap">
-        <div 
-            class="product-item"
-            v-for="product in products"
-            :key="product.id"
-        >
-            <img :src="product.imageName" />
-            <h3 class="product-name">{{ product.name }}</h3>
-            <p class="product-price">{{ product.price }}</p>
-            <router-link :to="'products' + '/' + product.id">
-                <button>View Details</button>
-            </router-link>
-        </div>
-    </div>
-
+    <ProductsList :products="products"/>
 </template>
 
 <script setup>
 import { products } from '@/temp-data';
-
+import ProductsList from '../components/ProductsList.vue'
 </script>
