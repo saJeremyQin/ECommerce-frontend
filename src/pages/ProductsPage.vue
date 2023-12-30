7b98cd99ba3fad176feddd2eb97532d5ecf09bb3
saJeremyQin <343220868@qq.com>
2023-12-30 23:04:13 +1030

    use axios to get products

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -4,6 +4,28 @@
 </template>
 
 <script setup>
-import { products } from '@/temp-data';
+// import { products } from '@/temp-data';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const products = ref([]);
+
+onMounted(async () => {
+    try {
+        const response =  await axios.get('/api/products');
+        products.value = response.data;   
+
+    } catch (error) {
+        if(error.response) {
+            console.error('Server responded with an error:', error.response.status);
+        } else if(error.request) {
+            console.error('No response received from the server');
+        } else {
+            console.error('Error in request setup:', error.message);
+        }
+    }
+})
+
+
 import ProductsList from '../components/ProductsList.vue'
 </script>
