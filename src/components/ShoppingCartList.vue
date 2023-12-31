07c99faca15a1b0384db36d7a0616c75cbfddc4a
saJeremyQin <343220868@qq.com>
2023-12-31 11:47:37 +1030

    Complete removeFromCart

--- src/components/ShoppingCartList.vue
+++ src/components/ShoppingCartList.vue
@@ -9,11 +9,16 @@
                 <h3>{{ product.name }}</h3>
                 <p>{{ product.price }}</p>
             </div>
-            <button class="remove-button">Remove from Cart</button>
+            <button class="remove-button" @click="removeFromCart(product.id)">Remove from Cart</button>
         </div> 
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import eventBus from '@/utils/eventBus';
+const removeFromCart = (productId) => {
+    // console.log(id);
+    eventBus.emit('removeFrom-Cart',productId);
+} 
 defineProps(['products']);
 </script>
--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -12,8 +12,20 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
+import eventBus from '@/utils/eventBus';
 import ShoppingCartList from '@/components/ShoppingCartList.vue';
 const cartItems = ref([]);
+
+eventBus.on('removeFrom-Cart', async (productId) => {
+    // console.log('id is', id);
+    try {
+        const response = await axios.delete(`/api/users/12345/cart/${productId}`);
+        console.log(response.data); 
+        cartItems.value = response.data;    
+    } catch (error) {
+        console.log(error.message);   
+    }
+})
 
 onMounted(async () => {
     const response = await axios.get('/api/users/12345/cart');
